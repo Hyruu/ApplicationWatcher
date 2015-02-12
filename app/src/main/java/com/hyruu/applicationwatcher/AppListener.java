3240c536522c311f652adc45ee6c012ec646bfaf
Florian DEGARDINS <flo.degardins@wanadoo.fr>
2015-02-12 18:16:02 +0100

    Major improvements
    Cleaned some code, added notification from the service to the activity,
    added list to the activity

--- app/src/main/java/com/hyruu/applicationwatcher/AppListener.java
+++ app/src/main/java/com/hyruu/applicationwatcher/AppListener.java
@@ -7,44 +7,110 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.IBinder;
+import android.os.Parcel;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppListener extends Service {
+    private boolean isActivityActive = true;
+    private ArrayList<Intent> intentList;
+
     public AppListener() {
     }
 
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+    private final BroadcastReceiver package_receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+
             String action = intent.getAction();
-            Log.v("log", "Broadcast received");
+            Log.v("package_receiver", "Broadcast received : " + action);
 
-            final PackageManager pm = getApplicationContext().getPackageManager();
-            ApplicationInfo ai;
-            String applicationName;
-            try {
-                ai = pm.getApplicationInfo(intent.getData().getSchemeSpecificPart(), PackageManager.GET_UNINSTALLED_PACKAGES);
-                applicationName = (String) pm.getApplicationLabel(ai);
-                Log.v("log", "Name found");
-            } catch (final PackageManager.NameNotFoundException e) {
-                applicationName = intent.getData().getSchemeSpecificPart();
-                ai = null;
-                Log.v("log", "Name not found");
-            }
-            Log.v("log", applicationName);
-
-            if(action.equals("android.intent.action.PACKAGE_ADDED")){
-                Log.v("log", "App installed : " + intent.getData().getSchemeSpecificPart());
-                Toast.makeText(context, "PACKAGE_ADDED", Toast.LENGTH_SHORT).show();
-            }
-            else if(action.equals("android.intent.action.PACKAGE_REMOVED")){
-                Log.v("log", "App removed : " + intent.getData().getSchemeSpecificPart());
-                Toast.makeText(context, "PACKAGE_REMOVED", Toast.LENGTH_SHORT).show();
+            if (isActivityActive) {
+                sendIntent(context, intent);
+            } else {
+                intentList.add(intent);
             }
         }
     };
+
+    private final BroadcastReceiver activity_receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.v("activity_receiver", "Broadcast received : " + action);
+
+            if(action.equals("com.hyruu.applicationwatcher.ACTIVITY_PAUSED")){
+                Log.v("activity_receiver", "Activity paused");
+                isActivityActive = false;
+            }
+            else if(action.equals("com.hyruu.applicationwatcher.ACTIVITY_RESUMED")){
+                Log.v("activity_receiver", "Activity resumed");
+                isActivityActive = true;
+
+                // Send all the intents stored during the pause to the activity
+                sendAllIntents(context);
+            }
+        }
+    };
+
+    private void sendIntent(Context context, Intent intent) {
+
+        // Get the Package Manager to get the application name
+        // TODO Replace this part with a Hash Table
+        final PackageManager pm = getApplicationContext().getPackageManager();
+        ApplicationInfo ai;
+        String applicationName;
+
+        // Try to get the application name
+        // TODO Database of the names since we can't get it after removal
+        try {
+            ai = pm.getApplicationInfo(intent.getData().getSchemeSpecificPart(), PackageManager.GET_UNINSTALLED_PACKAGES);
+            applicationName = (String) pm.getApplicationLabel(ai);
+            Log.v("sendIntent", "Name found");
+        } catch (final PackageManager.NameNotFoundException e) {
+            // Application name cannot be found, probably because application information have been deleted
+            applicationName = intent.getData().getSchemeSpecificPart();
+            ai = null;
+            Log.v("sendIntent", "Name not found");
+        }
+        Log.v("sendIntent", applicationName);
+
+        if(intent.getAction().equals("android.intent.action.PACKAGE_ADDED")){
+            Log.v("sendIntent", "App installed : " + intent.getData().getSchemeSpecificPart());
+
+            // Notify the user about the package
+            Toast.makeText(context, "PACKAGE_ADDED", Toast.LENGTH_SHORT).show();
+            // Notify the activity
+            Intent message = new Intent("com.hyruu.applicationwatcher.PACKAGE_ADDED");
+            message.putExtra("Application Name", applicationName);
+            sendBroadcast(message);
+        }
+        else if(intent.getAction().equals("android.intent.action.PACKAGE_REMOVED")){
+            Log.v("sendIntent", "App removed : " + intent.getData().getSchemeSpecificPart());
+
+            // Notify the user about the package
+            Toast.makeText(context, "PACKAGE_REMOVED", Toast.LENGTH_SHORT).show();
+            // Notify the activity
+            Intent message = new Intent("com.hyruu.applicationwatcher.PACKAGE_REMOVED");
+            message.putExtra("Application Name", applicationName);
+            sendBroadcast(message);
+        }
+    }
+
+    private void sendAllIntents(Context context) {
+
+        // Send all the stored intents to the user, and then clear the list
+        for (int i=0; i<intentList.size(); i++) {
+            sendIntent(context, intentList.get(i));
+        }
+        intentList.clear();
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -54,34 +120,45 @@ public class AppListener extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
         // handleCommand(intent);
-        // We want this service to continue running until it is explicitly
-        // stopped, so return sticky.
-        Log.v("log", "Service started");
+        Log.v("AppListener", "Service started");
 
-        //TODO Watch for application updates
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("android.intent.action.PACKAGE_ADDED");
-        filter.addAction("android.intent.action.PACKAGE_REMOVED");
-        filter.addDataScheme("package");
+        // Initialize the intent queue (for when the activity is paused)
+        intentList = new ArrayList<Intent>();
 
-        registerReceiver(receiver, filter);
+        // Register the BroadcastReceiver for package information
+        IntentFilter package_filter = new IntentFilter();
+        package_filter.addAction("android.intent.action.PACKAGE_ADDED");
+        package_filter.addAction("android.intent.action.PACKAGE_REMOVED");
+        package_filter.addDataScheme("package");
+        registerReceiver(package_receiver, package_filter);
 
+        // Register the BroadcastReceiver for activity information
+        IntentFilter activity_filter = new IntentFilter();
+        activity_filter.addAction("com.hyruu.applicationwatcher.ACTIVITY_PAUSED");
+        activity_filter.addAction("com.hyruu.applicationwatcher.ACTIVITY_RESUMED");
+        registerReceiver(activity_receiver, activity_filter);
 
-        //TODO Pop Toasts for the user and return the info to the app
+        //sendBroadcast(new Intent("com.hyruu.applicationwatcher.PACKAGE_ADDED", Uri.parse("test")));
 
-        // Tell the user we stopped.
+        // Tell the user we started the service
         Toast.makeText(this, "Service started", Toast.LENGTH_SHORT).show();
 
+        // We want this service to continue running until it is explicitly
+        // stopped, so return sticky.
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
 
-        unregisterReceiver(receiver);
-        // Tell the user we stopped.
+        // Unregister the receivers
+        unregisterReceiver(package_receiver);
+        unregisterReceiver(activity_receiver);
+
+        // Tell the user we stopped the service
         Toast.makeText(this, "Service stopped", Toast.LENGTH_SHORT).show();
-        Log.v("log", "Service stopped");
+        Log.v("AppListener", "Service stopped");
     }
 }
--- app/src/main/java/com/hyruu/applicationwatcher/History.java
+++ app/src/main/java/com/hyruu/applicationwatcher/History.java
@@ -1,22 +1,121 @@
 package com.hyruu.applicationwatcher;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class History extends ActionBarActivity {
+
+    ArrayList<String> list_events;
+    ArrayAdapter<String> adapter;
+    ListView listview;
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Log.v("receiver", "Broadcast from service : " + intent.getAction());
+            Log.v("receiver", intent.getStringExtra("Application Name"));
+
+            String action = "";
+            if (intent.getAction().equals("com.hyruu.applicationwatcher.PACKAGE_ADDED")) {
+                action = "Installed";
+            } else if (intent.getAction().equals("com.hyruu.applicationwatcher.PACKAGE_REMOVED")) {
+                action = "Removed";
+            }
+
+            addListElement(action + " : " + intent.getStringExtra("Application Name"));
+        }
+    };
+
+    private void addListElement(String element) {
+        // Add the element to the data list
+        list_events.add(element);
+        // Notify the adapter that the list changed
+        adapter.notifyDataSetChanged();
+    }
+
+    public void toggleAppListener(View view) {
+
+        //Check the toggle button state
+        boolean isToggleOn = ((ToggleButton) view).isChecked();
+
+        if (isToggleOn) {
+            // Start the service
+            startService(new Intent(this, AppListener.class));
+        } else {
+            // Stop the service
+            stopService(new Intent(this, AppListener.class));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        // Initialize the event list
+        list_events = new ArrayList<String>();
+        listview = (ListView) findViewById(R.id.listView);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                list_events);
+        listview.setAdapter(adapter);
     }
 
+    @Override
+    public void onDestroy() {
+
+        // Stop the service when the application is definitively closed
+        stopService(new Intent(this, AppListener.class));
+
+        // Don't forget to call the original onDestroy()
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Notify the service the activity is resumed and have it send all the stored information
+        sendBroadcast(new Intent("com.hyruu.applicationwatcher.ACTIVITY_RESUMED"));
+
+        // Register the receiver again
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.hyruu.applicationwatcher.PACKAGE_ADDED");
+        filter.addAction("com.hyruu.applicationwatcher.PACKAGE_REMOVED");
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+
+        // Pause the receiver, actually unregistering it
+        unregisterReceiver(receiver);
+
+        // Notify the service the activity is paused and will not receive any sent information
+        sendBroadcast(new Intent("com.hyruu.applicationwatcher.ACTIVITY_PAUSED"));
+
+        super.onPause();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,25 +139,28 @@ public class History extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void toggleAppListener(View view) {
+    private class StableArrayAdapter extends ArrayAdapter<String> {
 
-        //Check the toggle button state
-        boolean isToggleOn = ((ToggleButton) view).isChecked();
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
 
-        if (isToggleOn) {
-            // Start the service
-            startService(new Intent(this, AppListener.class));
-        } else {
-            // Stop the service
-            stopService(new Intent(this, AppListener.class));
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
         }
-    }
 
-    @Override
-    public void onDestroy() {
-        // Stop the service when the application is definitively closed
-        stopService(new Intent(this, AppListener.class));
-        // Don't forget to call the original onDestroy()
-        super.onDestroy();
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
